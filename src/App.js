73f697d823ac4e1c66ddf475c565c4c461518135
edmichel2002 <eddymichel2002@gmail.com>
2025-07-21 21:04:07 +0200

    test

--- src/App.js
+++ src/App.js
@@ -7,8 +7,16 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Bienvenue sur Formia App&nbsp;! Modifie <code>src/App.js</code> pour commencer.
         </p>
+        <a
+          className="App-link"
+          href="https://github.com/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Découvrir GitHub
+        </a>
         <a
           className="App-link"
           href="https://reactjs.org"
